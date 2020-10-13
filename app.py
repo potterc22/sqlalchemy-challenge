dff1952c538063feec84aa9e31c98a3255b5886d
Cory Potter <pottercory22@gmail.com>
2020-10-12 19:20:13 -0500

    Added start and end pages

--- app.py
+++ app.py
@@ -35,19 +35,19 @@ def home():
         f"Available Routes:<br/>"
         f"<br/>"
         f"/api/v1.0/precipitation<br/>"
-        f"This will display the last 12 months of precipitation data"
+        f"This will display the last 12 months of precipitation data<br/>"
         f"<br/>"
         f"/api/v1.0/stations<br/>"
-        f"This will display the list of weather stations"
+        f"This will display the list of weather stations<br/>"
         f"<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"This will display the last years worth of temperature observations from station USC00519281"
+        f"This will display the last years worth of temperature observations from station USC00519281<br/>"
         f"<br/>"
         f"/api/v1.0/start<br/>"
-        f"Given a start date, this will display the min, avg and max temperature for all dates greater than and equal to the start date"
+        f"Given a start date, this will display the min, avg and max temperature for all dates greater than and equal to the start date<br/>"
         f"<br/>"
         f"/api/v1.0/start/end<br/>"
-        f"Given a start and end date, this will display the min, avg, and max temperature for dates between the start and end date inclusively"
+        f"Given a start and end date, this will display the min, avg, and max temperature for dates between the start and end date inclusively<br/>"
     )
 
 @app.route("/api/v1.0/precipitation")
@@ -84,7 +84,10 @@ def tobs():
     # create our session from python to the DB
     session = Session(engine)
     # Perform query then close connection
-    results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.station == 'USC00519281').filter(Measurement.date >= '2016-08-23').group_by(Measurement.date).order_by(Measurement.date).all()
+    results = (session.query(Measurement.date, Measurement.tobs).
+        filter(Measurement.station == 'USC00519281').
+        filter(Measurement.date >= '2016-08-23').
+        group_by(Measurement.date).order_by(Measurement.date)).all()
     session.close()
 
     # Create a dictionary from the query output
@@ -97,6 +100,42 @@ def tobs():
     return jsonify(tobs_results)
 
 @app.route("/api/v1.0/<start>")
+def start_date(start):
+    # create our session from python to the DB
+    session = Session(engine)
+    # Perform query then close connection
+    results = (session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start)).all()
+    session.close()
+
+    # Create a dictionary from the query output
+    stats = []
+    for minimum, average, maximum in results:
+        stat_dict = {}
+        stat_dict['Min'] = minimum
+        stat_dict['Avg'] = average
+        stat_dict['Max'] = maximum
+        stats.append(stat_dict)
+    return jsonify(stats)
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start, end):
+    # create our session from python to the DB
+    session = Session(engine)
+    # Perform query then close connection
+    results = (session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).filter(Measurement.date <= end)).all()
+    session.close()
+
+    # Create a dictionary from the query output
+    stats = []
+    for minimum, average, maximum in results:
+        stat_dict = {}
+        stat_dict['Min'] = minimum
+        stat_dict['Avg'] = average
+        stat_dict['Max'] = maximum
+        stats.append(stat_dict)
+    return jsonify(stats)
 
 if __name__ == '__main__':
     app.run(debug=True)
